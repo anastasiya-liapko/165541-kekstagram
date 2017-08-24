034c8920ff156b5a593e44b9d9c36ca32ed1ad59
Anastasiya Liapko <anastasiya.liapko@gmail.com>
2017-08-25 00:19:12 +0300

    милый DOM

--- js/pictures.js
+++ js/pictures.js
@@ -32,18 +32,10 @@ var getComment = function () {
   return comment;
 };
 
-var createObjects = function (url, likes, comment) {
-  var objects = [];
-  for (var i = 0; i < 25; i++) {
-    objects[i] = [url[i], likes[i], comment[i]];
-  }
-  return objects;
-};
-
-var createPictures = function (objects) {
+var createPictures = function (url, likes, comment) {
   var pictures = [];
   for (var i = 0; i < 25; i++) {
-    pictures[i] = objects[i];
+    pictures[i] = [url[i], likes[i], comment[i]];
   }
   return pictures;
 };
@@ -51,10 +43,7 @@ var createPictures = function (objects) {
 var url = getUrl(1, 25);
 var likes = getLikes(15, 200);
 var comment = getComment(comments);
-var objects = createObjects(url, likes, comment);
-var pictures = createPictures(objects);
-
-
+var pictures = createPictures(url, likes, comment);
 
 var picturesList = document.querySelector('.pictures');
 var pictureTemplate = document.querySelector('#picture-template').content.querySelector('.picture');
@@ -62,19 +51,14 @@ var galleryOverlay = document.querySelector('.gallery-overlay');
 
 var renderPictures = function () {
   var picturesElement = pictureTemplate.cloneNode(true);
-  for (var i = 0; i < pictures.length; i++) {
-    picturesElement.querySelectorAll('img').src = pictures[i].objects[i].url[i];
-    picturesElement.querySelector('.picture-likes').textContent = pictures[i].objects[i].likes[i];
-    picturesElement.querySelector('.picture-comments').textContent = pictures[i].objects[i].comments[i];
-  }
+  picturesElement.querySelectorAll('img').src = pictures[i].url;
+  picturesElement.querySelector('.picture-likes').textContent = pictures[i].likes;
+  picturesElement.querySelector('.picture-comments').textContent = pictures[i].comment;
   return picturesElement;
 };
-
-renderPictures(pictures, objects);
-
 var fragment = document.createDocumentFragment();
 for (var i = 0; i < pictures.length; i++) {
-  fragment.appendChild(renderPictures(pictures.getUrl, pictures.getLikes, pictures.comments));
+  fragment.appendChild(renderPictures(pictures[i]));
 }
 picturesList.appendChild(fragment);
 
@@ -83,8 +67,10 @@ document.querySelector('.gallery-overlay').classList.remove('hidden');
 
 var renderGallery = function () {
   var galleryElement = pictureTemplate.cloneNode(true);
-  galleryElement.querySelector('.gallery-overlay-image').src = pictures.getUrl(1);
-  galleryElement.querySelector('.likes-count').textContent = pictures.getLikes(15, 200);
-  galleryElement.querySelector('.comments-count').textContent = pictures.comment;
+  galleryElement.querySelector('.gallery-overlay-image').src = pictures[0].url;
+  galleryElement.querySelector('.likes-count').textContent = pictures[0].likes;
+  galleryElement.querySelector('.comments-count').textContent = pictures[0].comment;
   galleryOverlay.appendChild(galleryElement);
 };
+
+renderGallery(pictures[0]);
